7b724813ad1528f479a9db810dd84ed4621154a6
DBAN <dban7171@gmail.com>
2021-10-17 15:09:15 +0900

    Queue는 가장 먼저 넣는 데이터가 먼저 나온다. 스택과 전혀 다르다. 하지만 스택과 똑같이 진행되는 lifo방식과 우선 순위에 따라서 보여주는 priority가 존재한다.

--- 4Queue.py
+++ 4Queue.py
@@ -1,11 +1,51 @@
 import queue
 
-data_queue = queue.Queue()
+# data_queue = queue.Queue()
+#
+# data_queue.put("123123")
+# data_queue.put(789456)
+# print(data_queue.qsize())
+# print(data_queue.get())
+# print(data_queue.qsize())
+# print(data_queue.get())
+# print(data_queue.qsize())
+#
+# lifo = queue.LifoQueue() # last in first out
+# lifo.put("qqqq")
+# lifo.put(1234)
+# print(lifo.qsize())
+# print(lifo.get())
+# print(lifo.qsize())
+# print(lifo.get())
+#
+# pri = queue.PriorityQueue()
+# pri.put((1,"어"))
+# pri.put((2,"ㅇㅇ"))
+# pri.put((5,"4124124"))
+# pri.put((0.1,"22222"))
+# print(pri.qsize())
+# print(pri.get())
+# print(pri.qsize())
+# print(pri.get())
+# print(pri.qsize())
+# print(pri.get())
+# print(pri.qsize())
+# print(pri.get())
 
-data_queue.put("123123")
-data_queue.put(789456)
-print(data_queue.qsize())
-print(data_queue.get())
-print(data_queue.qsize())
-print(data_queue.get())
-print(data_queue.qsize())
+que = list()
+def enqueue(data):
+    que.append(data)
+def dequeue():
+    data = que[0]
+    del que[0]
+    return data
+
+for i in range(10):
+    enqueue(i)
+print(que)
+print(len(que))
+print(dequeue())
+print(que)
+print(dequeue())
+print(que)
+print(dequeue())
