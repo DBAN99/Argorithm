03d0109adf15dc4e8c3ae6c18b56731f5fc84603
DBAN <dban7171@gmail.com>
2021-10-22 16:24:08 +0900

    quick sort 다시 확인 하기

--- 1-1QuickSort.py
+++ 1-1QuickSort.py
@@ -1,34 +1,23 @@
+def quick_sort(arr):
 
+    if len(arr) <= 1:
+        return arr
 
-array = [9,4,5,6,7,8,3,2,1]
-def quick_sort(array, start, end):
-    if start >= end: # 원소가 1개인 경우 종료
-        return
+    pivot = arr[len(arr) // 2]
 
-    pivot = start # 피벗은 첫 번째 원소
-    left = start + 1
-    right = end
+    less_arr, same_arr, more_arr = [], [], []
 
-    while(left <= right):
-        # 피벗보다 큰 데이터를 찾을 때까지 반복
-        while(left <= end and array[left] <= array[pivot]):
+    for num in arr:
+        if num < pivot:
+            less_arr.append(num)
 
-            left += 1
-        # 피벗보다 작은 데이터를 찾을 때까지 반복
+        elif num > pivot:
+            more_arr.append(num)
 
-        while(right > start and array[right] >= array[pivot]):
-            right -= 1
+        else:
+            same_arr.append(num)
 
-        if(left > right): # 엇갈렸다면 작은 데이터와 피벗을 교체
-            array[right], array[pivot] = array[pivot], array[right]
+    return quick_sort(less_arr) + same_arr + quick_sort(more_arr)
 
-        else: # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
-            array[left], array[right] = array[right], array[left]
-
-    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
-
-    quick_sort(array, start, right - 1)
-    quick_sort(array, right + 1, end)
-
-quick_sort(array, 0, len(array) - 1)
-print(array)
+arr = [4,6,9,8,7,5,3,2,1]
+print(quick_sort(arr))
