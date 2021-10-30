f8c5b3fffaa32adf94b4963315d7abb6b046c9b4
DBAN <dban7171@gmail.com>
2021-10-30 11:42:34 +0900

    백준 문제 풀이

---
+++ 9 divisor&while.py
@@ -0,0 +1,27 @@
+a,b =map(int,input().split())
+c = []
+for i in range(1,a):
+    if a % i == 0:
+        c.append(i)
+
+if b > len(c):
+    print(0)
+else:
+    print(c[b-1])
+
+a = int(input())
+num = a
+cnt = 0
+# ---------------------------------
+while True:
+    b = num // 10 # 2
+    c = num % 10  # 6
+    d = (b + c) % 10  # 8
+    num = (c * 10) + d
+    cnt +=1
+
+    if num == a:
+        break
+print(cnt)
+
+print( 1//3)
